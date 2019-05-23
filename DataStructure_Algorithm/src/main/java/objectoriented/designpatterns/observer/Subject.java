2bb3edb003a56c9290d766a602e63431c0fa3ef9
fajr23 <mobileuygulama@gmail.com>
2019-05-24 02:07:06 +0300

    observer pattern

---
+++ DataStructure_Algorithm/src/main/java/objectoriented/designpatterns/observer/Subject.java
@@ -0,0 +1,11 @@
+package objectoriented.designpatterns.observer;
+
+public interface Subject {
+
+    void registerObserver(Observer o);
+
+    void removeObserver(Observer o);
+
+    void notifyObservers();
+
+}
---
+++ DataStructure_Algorithm/src/main/java/objectoriented/designpatterns/observer/WeatherData.java
@@ -0,0 +1,74 @@
+package objectoriented.designpatterns.observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WeatherData implements Subject {
+    private List<Observer> observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData() {
+        this.observers = new ArrayList();
+    }
+
+    public void measurementChanged() {
+
+        notifyObservers();
+
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public float getPressure() {
+        return pressure;
+    }
+
+
+    public void setObservers(List<Observer> observers) {
+        this.observers = observers;
+    }
+
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
+    }
+
+    public void setHumidity(float humidity) {
+        this.humidity = humidity;
+    }
+
+    public void setPressure(float pressure) {
+        this.pressure = pressure;
+    }
+
+    @Override
+    public void registerObserver(Observer o) {
+        observers.add(o);
+
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        observers.remove(o);
+    }
+
+    @Override
+    public void notifyObservers() {
+
+        observers.stream().forEach(item -> item.update(getTemperature(), getHumidity(), getPressure()));
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementChanged();
+    }
+}
