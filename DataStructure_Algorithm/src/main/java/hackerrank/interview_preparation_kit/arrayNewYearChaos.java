bf05a443ba351be0d578ac2a200e22ac87ccf849
fajr23 <mobileuygulama@gmail.com>
2019-06-07 22:00:07 +0300

    interview preparation kit

--- DataStructure_Algorithm/src/main/java/hackerrank/interview_preparation_kit/arrayNewYearChaos.java
+++ DataStructure_Algorithm/src/main/java/hackerrank/interview_preparation_kit/arrayNewYearChaos.java
@@ -16,7 +16,20 @@ public class arrayNewYearChaos {
         // Complete the minimumBribes function below.
         static void minimumBribes(int[] q) {
 
+            int result=0;
 
+            for(int i=q.length-1; i>=0; i--){
+
+                if ((q[i])-(i+1)>2){
+                    System.out.println("Too chaotic");
+                    return;
+                }
+
+                for(int j=Math.max(0, q[i]-2);j<i;j++){
+                    if(q[j]>q[i]) result++;
+                }
+            }
+            System.out.println(result);
         }
 
         private static final Scanner scanner = new Scanner(System.in);
