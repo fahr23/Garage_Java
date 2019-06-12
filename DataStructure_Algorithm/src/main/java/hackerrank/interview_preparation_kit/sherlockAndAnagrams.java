20e55b20f482c877213b96d6cd671cc07615ee97
fajr23 <mobileuygulama@gmail.com>
2019-06-12 23:01:24 +0300

    interview preparation kit

---
+++ DataStructure_Algorithm/src/main/java/hackerrank/interview_preparation_kit/sherlockAndAnagrams.java
@@ -0,0 +1,34 @@
+package hackerrank.interview_preparation_kit;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class sherlockAndAnagrams {
+
+
+    // Complete the sherlockAndAnagrams function below.
+    static int sherlockAndAnagrams(String s) {
+
+        return 0;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            int result = sherlockAndAnagrams(s);
+
+        }
+
+
+        scanner.close();
+    }
+
+}
