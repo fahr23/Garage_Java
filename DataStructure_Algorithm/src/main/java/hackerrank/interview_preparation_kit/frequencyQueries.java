06e6ba33c4ca0c9c81347a4757a6bad5563befa8
fajr23 <mobileuygulama@gmail.com>
2019-06-22 23:57:50 +0300

    interview preparation kit

--- DataStructure_Algorithm/src/main/java/hackerrank/interview_preparation_kit/frequencyQueries.java
+++ DataStructure_Algorithm/src/main/java/hackerrank/interview_preparation_kit/frequencyQueries.java
@@ -21,12 +21,46 @@ public class frequencyQueries {
         // Complete the freqQuery function below.
         static List<Integer> freqQuery(List<List<Integer>> queries) {
 
-            return null;
+
+            List<Integer> response = new ArrayList();
+            List<Integer> result = new ArrayList();
+
+            Map<Integer, Integer> mapfrequency = new HashMap<>();
+
+            queries.stream().forEach(e -> {
+                        System.out.println(e.get(0));
+                        if (e.get(0) == 1) {
+                            //response.add(e.get(1));
+                            mapfrequency.merge(e.get(1), 1, Integer::sum);
+                        } else if (e.get(0) == 2) {
+                            Integer integer = mapfrequency.get(e.get(1));
+                            if(integer!=null && integer>0){
+                                mapfrequency.merge(e.get(1), -1, Integer::sum);
+                            }
+                            //if (response.remove(e.get(1))) {
+                            //}
+
+                        } else if (e.get(0) == 3) {
+
+                            Optional<Integer> first = mapfrequency.values().stream().filter(v -> v == e.get(1)).findFirst();
+
+                            if (first.isPresent())
+                                result.add(1);
+                            else
+                                result.add(0);
+                        }
+                    }
+            );
+
+
+            return result;
+
+
         }
 
         public static void main(String[] args) throws IOException {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("fa"));
 
             int q = Integer.parseInt(bufferedReader.readLine().trim());
 
